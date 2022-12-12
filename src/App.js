70578f6becfb3635c36a497159819051a6fa2de7
Asael Hernández <asael.dev@gmail.com>
2022-12-12 14:49:59 -0600

    feat: 1) Added SignUp, SignIn, and Home pages. 2) Installed react-router, react-router-dom, and @mui/icons-material dependencies. 3) Added the website's logo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import EnhancedTable from "./components/EnhancedTable";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material/styles";
+import { Routes, Route } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#757ce8",
+      main: "#fafafa",
+      dark: "#002884",
+      contrastText: "#000000",
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#000",
+    },
+  },
+  typography: {
+    primary: {
+      main: "#000000",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/SignIn" element={<SignIn />}></Route>
+          <Route path="/SignUp" element={<SignUp />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
